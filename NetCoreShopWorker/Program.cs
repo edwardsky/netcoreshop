4140387c9534f69f3fe57946fa350a5c3b3cd15b
Edward Sablin <edwardsky@gmail.com>
2020-12-16 09:29:34 +0300

    add worker project

---
+++ NetCoreShopWorker/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NetCoreShopWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NetCoreShop Worker");
+            
+            // TODO load appsettings
+            // TODO connect to RabbitMQ
+            // TODO Get job from queue
+            // TODO Run job
+        }
+    }
+}
